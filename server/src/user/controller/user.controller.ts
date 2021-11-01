3b22ea6885d0c7f8d5bde76c184571256e3f65ae
kykapple <kykapple@naver.com>
2021-11-01 21:18:04 +0900

    fix: 사용자의 모든 artwork 조회 API 반환 타입 지정

--- server/src/user/controller/user.controller.ts
+++ server/src/user/controller/user.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { UserService } from '../service/user.service';
+import { Artwork } from '../../artwork/artwork.entity';
 
 @Controller('/users')
 export class UserController {
     constructor(private userService: UserService) {}
 
     @Get('/:userId/artworks')
-    getAllUsersArtworks(@Param('userId', ParseIntPipe) userId: number) {
+    getAllUsersArtworks(@Param('userId', ParseIntPipe) userId: number): Promise<Artwork[]> {
         return this.userService.getAllUsersArtworks(userId);
     }
 
