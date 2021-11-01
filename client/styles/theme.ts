1c30fe4f4a2dde72cd9ea6f7669cb76297054cb6
illlama <beatlemaniakih@gmail.com>
2021-11-01 15:34:38 +0900

    feat:theme와 common 추가 - #72

---
+++ client/styles/theme.ts
@@ -0,0 +1,11 @@
+const theme = {
+    color: {
+        black: '#1C1B29',
+        blackLight: '#292841',
+        placeholder: '#757575',
+        gray1: '#C2C2C2',
+        main: '#DC6B6B',
+    },
+};
+
+export default theme;
