dc45761b6876905889126b6c7b26e32962c24eb1
kykapple <kykapple@naver.com>
2021-11-01 18:00:43 +0900

    feat: Global Prefix 설정 서버 global prefix를 설정하여 API 접근 경로의 시작을 /api로 하였다.

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+
+  app.setGlobalPrefix('/api');
+
+  await app.listen(PORT);
 }
+
 bootstrap();
