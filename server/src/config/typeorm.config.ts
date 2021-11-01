3700849bb3871bcc117a67562a26f0bb004f0fbd
kykapple <kykapple@naver.com>
2021-11-01 17:42:57 +0900

    fix: TypeORM 설정 파일 환경 변수 등록

--- server/src/config/typeorm.config.ts
+++ server/src/config/typeorm.config.ts
@@ -1,14 +1,13 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 import { SnakeNamingStrategy } from "typeorm-naming-strategies";
 
-
 export const typeORMConfig: TypeOrmModuleOptions = {
     type: 'mysql',
-    host: '',
+    host: process.env.DB_HOST,
     port: 3306,
-    username: '',
-    password: '',
-    database: '',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: false,
     namingStrategy: new SnakeNamingStrategy(),
