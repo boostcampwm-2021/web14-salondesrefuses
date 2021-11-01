fafc74aa10185d3b6531468f035d0236096c50a2
kykapple <kykapple@naver.com>
2021-11-01 19:28:03 +0900

    feat: 랜덤 auction artwork 조회 API 구현 - #58

---
+++ server/src/artWork/artWork.repository.ts
@@ -0,0 +1,16 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Artwork } from "./artwork.entity";
+import { ArtworkStatus } from "./artwork.status.enum";
+
+@EntityRepository(Artwork)
+export class ArtworkRepository extends Repository<Artwork> {
+
+    async getRandomAuctionArtworks(): Promise<Artwork[]> {
+        return await this.createQueryBuilder('artwork')
+            .where('artwork.status = :status', { status: ArtworkStatus.InBid })
+            .orderBy('RAND()')
+            .limit(3)
+            .getMany();
+    }
+
+}
