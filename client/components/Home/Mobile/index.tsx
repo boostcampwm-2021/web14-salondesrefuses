8989de9a15051f5871bf5d377e8dcc1649329ff2
illlama <beatlemaniakih@gmail.com>
2021-11-02 18:16:55 +0900

    fix: 감싸는 div 넣고, fragment 사용도 하고 - #87

--- client/components/Home/Mobile/index.tsx
+++ client/components/Home/Mobile/index.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import { Container, LogoContainer, Text, Hr, Copyright } from './styles';
 import Image from 'next/image';
-import Logo from '@assets/images/tmpLogo.png';
+import Logo from '@assets/images/logo.png';
 
 const Mobile = () => {
     return (
-        <Container>
-            <div>
-                <LogoContainer>
-                    <Image src={Logo} alt="logo" />
-                    <p>Salon des Refusés</p>
-                </LogoContainer>
-                <Text>
-                    벽전은 모바일을 준비중입니다. <br />
-                    PC로 접속하여 주시면 감사하겠습니다.
-                </Text>
-                <Hr />
-                <Copyright>© salon des refuses, 2021</Copyright>
-            </div>
-        </Container>
+        <div>
+            <Container>
+                <div>
+                    <LogoContainer>
+                        <Image src={Logo} alt="logo" />
+                        <p>Salon des Refusés</p>
+                    </LogoContainer>
+                    <Text>
+                        벽전은 모바일을 준비중입니다. <br />
+                        PC로 접속하여 주시면 감사하겠습니다.
+                    </Text>
+                    <Hr />
+                    <Copyright>© salon des refuses, 2021</Copyright>
+                </div>
+            </Container>
+        </div>
     );
 };
 
