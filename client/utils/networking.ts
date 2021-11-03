0c605dd4f7a2e332876d80294180937335c4ba3b
kykapple <kykapple@naver.com>
2021-11-04 01:13:40 +0900

    fix: api server url 환경 변수 오류 해결 - next.config.js파일에 오탈자 수정

--- client/utils/networking.ts
+++ client/utils/networking.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.API_SERVER_URL;
+const API_SERVER_URL = process.env.API_SERVER_URL;
 
 export const getAllArtworks = (userId: number) => {
-    return axios.get(`${API_BASE_URL}/users/${userId}/artworks`);
+    return axios.get(`${API_SERVER_URL}/users/${userId}/artworks`);
 };
 
 export const signIn = (code: string, strategy: string) => {
-    return axios.post(`http://localhost:3001/api/auth/signIn`, JSON.stringify({ code, strategy }), {
+    return axios.post(`${API_SERVER_URL}/auth/signIn`, JSON.stringify({ code, strategy }), {
         headers: { 'Content-Type': 'Application/JSON' },
         withCredentials: true
     });
