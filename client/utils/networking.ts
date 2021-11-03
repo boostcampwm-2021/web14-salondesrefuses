22f59e102df854d3637054641c7476a0119f7b42
kykapple <kykapple@naver.com>
2021-11-03 22:24:42 +0900

    feat: 구글 로그인 및 카카오 로그인 API 구현

--- client/utils/networking.ts
+++ client/utils/networking.ts
@@ -5,3 +5,17 @@ const API_BASE_URL = process.env.API_SERVER_URL;
 export const getAllArtworks = (userId: number) => {
     return axios.get(`${API_BASE_URL}/users/${userId}/artworks`);
 };
+
+export const signInWithGoogle = (code: string) => {
+    return axios.post(`${API_BASE_URL}/auth/google`, JSON.stringify({ code }), {
+        headers: { 'Content-Type': 'Application/JSON' },
+        withCredentials: true
+    });
+};
+
+export const signInWithKakao = (code: string) => {
+    return axios.post(`${API_BASE_URL}/auth/kakao`, JSON.stringify({ code }), {
+        headers: { 'Content-Type': 'Application/JSON' },
+        withCredentials: true
+    });
+};
