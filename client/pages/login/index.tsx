8159e5cc01cf3249fd632b926405c023c351c7a0
imnotmoon <imnotmoon@gmail.com>
2021-11-03 15:11:49 +0900

    feat: login 페이지 레이아웃 구성
    - 레이아웃 구성

---
+++ client/pages/login/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import Head from 'next/head';
+import Image from 'next/image';
+
+import sideImage from '@assets/images/login-side-test.png';
+import Login from '@components/Login';
+
+const LoginPage = () => {
+    return (
+        <Container>
+            <Head>
+                <title>벽전 - Salon des Refusés</title>
+                <meta name="description" content="벽전 로그인 페이지" />
+            </Head>
+            <Body>
+                <Image src={sideImage} alt="side image" />
+                <Login></Login>
+            </Body>
+        </Container>
+    );
+};
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: ${(props) => props.theme.color.background};
+`;
+
+const Body = styled.div`
+    height: 80vh;
+    width: 70vw;
+    max-width: 1000px;
+    background-color: white;
+    border-radius: 15px;
+    box-shadow: -2px 8px 24px 1px rgba(149, 157, 165, 0.2);
+    display: grid;
+    grid-template-columns: 5fr 6fr;
+
+    & img {
+        border-radius: 15px 0 0 15px;
+    }
+`;
+
+export default LoginPage;
--- client/styles/theme.ts
+++ client/styles/theme.ts
@@ -5,6 +5,7 @@ const theme = {
         placeholder: '#757575',
         gray1: '#C2C2C2',
         primary: '#6D8299',
+        background: '#F9F9F7',
     },
     font: {
         textEnXl: `28px "Montserrat"`,
