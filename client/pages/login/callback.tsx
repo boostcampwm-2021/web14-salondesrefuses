82244716f7d9e8c1a155274b99e9414edc6161b2
kykapple <kykapple@naver.com>
2021-11-03 22:23:38 +0900

    feat: 로그인 버튼 href 작성 및 로그인 callback 페이지 구현

---
+++ client/pages/login/callback.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect } from 'react';
+import styled from '@emotion/styled';
+import { useRouter } from 'next/router';
+import { ClipLoader } from 'react-spinners';
+import { signInWithGoogle, signInWithKakao } from '../../utils/networking';
+
+const LoginCallbackPage = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        let signIn = null;
+        const code: string | null = new URL(window.location.href)
+            .searchParams
+            .get('code');
+
+        (async () => {
+            if(window.location.href.includes('google')) {
+                signIn = await signInWithGoogle(code!);
+            } else {
+                signIn = await signInWithKakao(code!);
+            }
+
+            signIn ? router.push('/') : router.push('/login');
+        })();
+
+    }, []);
+
+    return (
+        <Container>
+            <LoadingSpinner>
+                <Notice>Loading...</Notice>
+                <ClipLoader size="100" color="#000"></ClipLoader>
+            </LoadingSpinner>
+        </Container>
+    );
+};
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const LoadingSpinner = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 200px;
+    margin: auto 0;
+`;
+
+const Notice = styled.div`
+    font-size: 30px;
+    font-weight: 700;
+    margin-bottom: 50px;
+    color: #000;
+`;
+
+export default LoginCallbackPage;
