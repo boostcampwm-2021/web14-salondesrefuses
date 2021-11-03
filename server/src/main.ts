ad61d5ff7aee6158de81aca42bf0669e78d8075c
kykapple <kykapple@naver.com>
2021-11-03 23:50:09 +0900

    chore: cookie 사용을 위한 cookieParser 설정

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
 dotenv.config();
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerUIConfig } from './config/swagger.config';
@@ -14,8 +15,9 @@ async function bootstrap() {
         swaggerUIConfig.options,
     );
 
-    app.enableCors({ origin: 'http://localhost:3000', credentials: true });
+    app.use(cookieParser());
     app.setGlobalPrefix('/api');
+    app.enableCors({ origin: 'http://localhost:3000', credentials: true });
     SwaggerModule.setup('api', app, document);
 
     await app.listen(PORT);
