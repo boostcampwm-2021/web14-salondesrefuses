a32f0bd4499c770446b838d0b1112fb2d5abf777
illlama <beatlemaniakih@gmail.com>
2021-11-04 17:06:29 +0900

    feat: getStaticProps로 page에서 데이터 페칭, 불필요한 로직 제거 -#67
    getStaticProps로 데이터 페칭함. page에서 해야함!!!
    그리고 DeviceSwitch 컴포넌트 제거해버리고 Index.tsx에서 처리함...

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,20 @@
 import type { NextPage } from 'next';
-import DeviceSwitch from '@components/common/DeviceSwitch';
+import { GetStaticProps } from 'next';
+
 import Mobile from '@components/Home/Mobile';
 import Pc from '@components/Home/Pc';
+import { isMobile } from 'utils/isMobile';
+import { getRandomExhibitions } from 'utils/networking';
 
-const Home: NextPage = (props) => {
-    return <DeviceSwitch Pc={Pc} Mobile={Mobile} props={props} />;
+const Home: NextPage = (data) => {
+    return isMobile() ? <Mobile /> : <Pc data={data} />;
+};
+export const getStaticProps: GetStaticProps = async () => {
+    const ExhibitionData = await (await getRandomExhibitions()).data;
+
+    return {
+        props: { ExhibitionData },
+    };
 };
 
 export default Home;
