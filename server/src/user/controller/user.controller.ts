ff59fc06dbe8ab5c3212103606dd3c9f0a4b30a8
Alencion <rhqnrdl625@gmail.com>
2021-11-04 21:28:31 +0900

    feat: 유저 컨트롤러 스웨거 추가 - #110

--- server/src/user/controller/user.controller.ts
+++ server/src/user/controller/user.controller.ts
@@ -1,16 +1,32 @@
-import { Controller, Get, Param, ParseIntPipe, UseGuards } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Param,
+    ParseIntPipe,
+    UseGuards,
+} from '@nestjs/common';
 import { UserService } from '../service/user.service';
 import { Artwork } from '../../artwork/artwork.entity';
 import { CustomAuthGuard } from '../../auth/guard/CustomAuthGuard';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+    getAllUsersArtworksApiOperation,
+    getAllUsersArtworksApiQuery,
+} from '../swagger';
 
 @UseGuards(CustomAuthGuard)
 @Controller('/users')
+@ApiTags('유저 컨트롤러')
 export class UserController {
     constructor(private userService: UserService) {}
 
     @Get('/:userId/artworks')
-    getAllUsersArtworks(@Param('userId', ParseIntPipe) userId: number): Promise<Artwork[]> {
+    @ApiOperation(getAllUsersArtworksApiOperation)
+    @ApiQuery(getAllUsersArtworksApiQuery)
+    @ApiResponse({ type: Artwork })
+    getAllUsersArtworks(
+        @Param('userId', ParseIntPipe) userId: number,
+    ): Promise<Artwork[]> {
         return this.userService.getAllUsersArtworks(userId);
     }
-
 }
---
+++ server/src/user/swagger/index.ts
@@ -0,0 +1,6 @@
+export const getAllUsersArtworksApiOperation = {
+    summary: '내 모든 작품 조회 API',
+    description: '작품 데이터를 DB에 저장한다.',
+};
+
+export const getAllUsersArtworksApiQuery = { name: 'userId', type: Number };
