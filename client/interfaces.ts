9df6d956acb2f2ed659c963113de76820865567c
imnotmoon <imnotmoon@gmail.com>
2021-11-04 16:42:39 +0900

    feat: 작품 추가 api 등록
    - 작품 추가 api 연결
    - 성공 시 메인 페이지로 라우팅

--- client/interfaces.ts
+++ client/interfaces.ts
@@ -2,3 +2,23 @@ export interface Artwork {
     id: number;
     imagePath: string;
 }
+
+export interface PostArtworkResponse {
+    id: number;
+    title: string;
+    type: string;
+    description: string;
+    status: string;
+    price?: string;
+    nftToken?: string;
+    originalImage: string;
+    croppedImage: string;
+    exhibitionId?: string;
+    auction?: Auction;
+}
+
+export interface Auction {
+    endAt: string;
+    id: number;
+    startAt: Date;
+}
