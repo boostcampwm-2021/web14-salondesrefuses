c732097e5ebb3ca03e96c932e69ec4b5afa025e7
imnotmoon <imnotmoon@gmail.com>
2021-11-04 17:50:18 +0900

    feat: auction list 페이지 carousel 추가
    - react-slick 사용해서 carousel 추가
    - 임시로 div 3개 넣고 테스트

--- client/pages/auction/index.tsx
+++ client/pages/auction/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import styled from '@emotion/styled';
+import Layout from '@components/common/Layout';
 
-const AuctionPage = () => {
-    return <div></div>;
+import AuctionCarousel from '@components/Auction/Carousel';
+
+const AuctionPage = ({ data }: any) => {
+    return (
+        <Container>
+            <Head>
+                <title>벽전 - Salon des Refusés</title>
+                <meta name="description" content="경매중인 작품 리스트" />
+            </Head>
+            <Layout>
+                <AuctionCarousel />
+            </Layout>
+        </Container>
+    );
 };
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    return {
+        props: {},
+    };
+};
+
+const Container = styled.div``;
 
 export default AuctionPage;
