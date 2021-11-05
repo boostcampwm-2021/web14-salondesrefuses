71148c27187d2461ffd5687b3e90cdf49c1ef161
illlama <beatlemaniakih@gmail.com>
2021-11-06 00:04:57 +0900

    feat: pages -> index.tsx에 getStaticProps를 통해 랜덤전시,경매 정보 받아오기 -#67
    아직 경매 정보 API는 준비되어있지 않기 때문에 더미데이터를 가져옴

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,18 +2,30 @@ import type { NextPage } from 'next';
 import { GetStaticProps } from 'next';
 
 import Mobile from '@components/Home/Mobile';
-import Pc from '@components/Home/Pc';
+import Pc from '@components/Home/Pc/Pc';
 import { isMobile } from 'utils/isMobile';
-import { getRandomExhibitions } from 'utils/networking';
+import { getRandomAuctions, getRandomExhibitions } from 'utils/networking';
+import {
+    fakeRandomAuctions,
+    randomAuctionType,
+    randomExhibitionType,
+} from 'constants/fakeDatas';
 
-const Home: NextPage = (data) => {
-    return isMobile() ? <Mobile /> : <Pc data={data} />;
+const Home: NextPage = (props) => {
+    return isMobile() ? <Mobile /> : <Pc props={props} />;
 };
 export const getStaticProps: GetStaticProps = async () => {
-    const ExhibitionData = await (await getRandomExhibitions()).data;
+    const ExhibitionsData = (await getRandomExhibitions()).data.map(
+        (exhibition: randomExhibitionType) => JSON.stringify(exhibition),
+    );
+
+    // const AuctionData = JSON.stringify((await getRandomAuctions()).data);
+    const AuctionsData = fakeRandomAuctions.map((auction: randomAuctionType) =>
+        JSON.stringify(auction),
+    );
 
     return {
-        props: { ExhibitionData },
+        props: { ExhibitionsData, AuctionsData },
     };
 };
 
