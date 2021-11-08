031f2045f0c09be27cf0cb8149e35c6ff5c07a76
imnotmoon <imnotmoon@gmail.com>
2021-11-08 17:31:48 +0900

    feat: Artwork 모달 내부 year, bidEnd, startPrice 검증 로직 추가
    - year, bidEnd, startPrice 잘못된 형식의 입력을 받을 경우 에러 메시지 출력
    - year, bidEnd, startPrice 로직 길어지는것 같아 커스텀 훅으로 분리, 파일은 별도로 분리하지 않음

--- client/components/Artwork/ArtworkModal/index.tsx
+++ client/components/Artwork/ArtworkModal/index.tsx
@@ -15,42 +15,36 @@ const ArtworkModal = ({
     position,
     setPosition,
 }: ArtworkModalProps) => {
-    const [checked, setChecked] = useState('artwork');
     const modalRef = useRef<HTMLDivElement | null>(null);
-
-    const [descriptionInput, setDescriptionInput] = useState('');
-    const [yearInput, setYearInput] = useState(date.getFullYear().toString());
-    const [bidEndInput, setBidEndIput] = useState('');
-
+    const [checked, setChecked] = useState('artwork');
+    const [description, setDescription] = useState('');
+    const { year, onChangeYear, validateYear } = useInputYear();
+    const { bidEnd, onChangeBidEnd, validateBidEnd } = useInputBidEnd();
+    const { price, onChangePrice, validatePrice } = useInputPrice();
     const onChangeDescription = (e: React.FormEvent) => {
-        setDescriptionInput((e.target as HTMLTextAreaElement).value);
-    };
-
-    const onChangeYear = (e: React.FormEvent) => {
-        setYearInput((e.target as HTMLInputElement).value);
-    };
-
-    const onChangeBidEnd = (e: React.FormEvent) => {
-        setBidEndIput((e.target as HTMLInputElement).value);
+        setDescription((e.target as HTMLTextAreaElement).value);
     };
 
     const onClickConfirm = () => {
         setData({
-            description: descriptionInput,
-            year: yearInput,
-            bidEnd: bidEndInput,
+            description,
+            year,
+            bidEnd: bidEnd ?? '',
         });
         setPosition('-59vh');
     };
 
     return (
         <Modal bottom={position} ref={modalRef}>
-            <ArtworkFilter checked={checked} setChecked={setChecked} />
+            <ArtworkFilter
+                checked={checked}
+                setChecked={(checked) => setChecked(checked)}
+            />
             <Form>
                 <span>Description</span>
                 <textarea
                     cols={10}
-                    value={descriptionInput}
+                    value={description}
                     onChange={onChangeDescription}
                 />
             </Form>
@@ -60,33 +54,88 @@ const ArtworkModal = ({
                         <span>Year</span>
                         <input
                             type="text"
-                            value={yearInput}
+                            value={year}
                             onChange={onChangeYear}
                         />
+                        {!validateYear && <p>input right format</p>}
                     </LightForm>
                     <LightForm>
                         <span>Bid End</span>
                         <input
                             type="text"
-                            value={bidEndInput}
+                            value={bidEnd}
                             onChange={onChangeBidEnd}
                             placeholder="yyyy-mm-dd"
                         />
+                        {!validateBidEnd && <p>input right format</p>}
                     </LightForm>
                     <LightForm>
                         <span>Start Price</span>
-                        <input
-                            type="text"
-                            value={bidEndInput}
-                            onChange={onChangeBidEnd}
-                            placeholder="xxx ETH"
-                        />
+                        <div>
+                            <div>
+                                <input
+                                    type="text"
+                                    value={price ?? 0}
+                                    onChange={onChangePrice}
+                                    placeholder={`${price} ETH`}
+                                />
+                                {!validatePrice && <p>input right format</p>}
+                            </div>
+                            <span>ETH</span>
+                        </div>
                     </LightForm>
                 </>
             )}
             <ConfirmButton onClick={onClickConfirm}>confirm</ConfirmButton>
         </Modal>
     );
+};
+
+const useInputYear = () => {
+    const [year, setYear] = useState(date.getFullYear().toString());
+    const [validateYear, setValidateYear] = useState(true);
+
+    const onChangeYear = (e: React.FormEvent) => {
+        const input = (e.target as HTMLInputElement).value;
+        if (!RegExp(/\d{4}/gi).test(input) && input.length > 0)
+            setValidateYear(false);
+        else setValidateYear(true);
+        setYear(input);
+    };
+
+    return { year, onChangeYear, validateYear };
+};
+
+const useInputBidEnd = () => {
+    const [bidEnd, setBidEnd] = useState('');
+    const [validateBidEnd, setValidateBidEnd] = useState(true);
+
+    const onChangeBidEnd = (e: React.FormEvent) => {
+        const input = (e.target as HTMLInputElement).value;
+        const regex = RegExp(
+            /^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/gi,
+        );
+        if (!regex.test(input) && input.length > 0) setValidateBidEnd(false);
+        else setValidateBidEnd(true);
+        setBidEnd(input);
+    };
+
+    return { bidEnd, onChangeBidEnd, validateBidEnd };
+};
+
+const useInputPrice = () => {
+    const [price, setPrice] = useState('0');
+    const [validatePrice, setValidatePrice] = useState(true);
+
+    const onChangePrice = (e: React.FormEvent) => {
+        const input = (e.target as HTMLInputElement).value;
+        const regex = RegExp(/[0-9.]+/gi);
+        if (!regex.test(input) && input.length > 0) setValidatePrice(false);
+        else setValidatePrice(true);
+        setPrice(input);
+    };
+
+    return { price, onChangePrice, validatePrice };
 };
 
 export default ArtworkModal;
