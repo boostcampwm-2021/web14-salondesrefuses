5f463b7e197090505811fc2c68d895f7d9403e71
imnotmoon <imnotmoon@gmail.com>
2021-11-10 14:09:00 +0900

    feat: react-color 모듈 추가

---
+++ client/pages/exhibition/edit.tsx
@@ -0,0 +1,12 @@
+import Editor from '@components/Exhibition/Editor';
+import React from 'react';
+
+const edit = () => {
+    return (
+        <div>
+            <Editor />
+        </div>
+    );
+};
+
+export default edit;
