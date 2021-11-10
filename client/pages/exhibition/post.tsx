4651dabd709843a039c0d60e824d71e51c1442e5
imnotmoon <imnotmoon@gmail.com>
2021-11-11 02:19:18 +0900

    refactor: exhibition/post 페이지 리팩토링
    - 입력 부분 분리(Form)
    - ArtworkSelector 분리

--- client/pages/exhibition/post.tsx
+++ client/pages/exhibition/post.tsx
@@ -1,72 +1,55 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import Head from 'next/head';
+
+import Form from '@components/Exhibition/Form';
+import ArtworkSelector from '@components/Exhibition/ArtworkSelector';
 import Layout from '@components/common/Layout';
-import LabelInput from '@components/Exhibition/LabelInput';
-import {
-    ArtworkSelector,
-    ArtworkSelectorHeader,
-    ArtworkSelectorWrapper,
-    ContentBody,
-    ContentHeader,
-    DataInput,
-    Description,
-    Input,
-    Label,
-    NextButton,
-    TextArea,
-    ThumbnailBox,
-    Title,
-} from '@components/Exhibition/style';
-import Link from 'next/link';
-import React, { useRef } from 'react';
+import { Description, NextButton } from '@components/Exhibition/style';
 
 const ExhibitionPostPage = () => {
-    const thumbnailRef = useRef<HTMLInputElement>(null);
     return (
-        <Layout>
-            <ContentHeader>
-                <Title>Hold Exhibition</Title>
-                <Description>나만의 전시회를 만들어 보세요!</Description>
-            </ContentHeader>
-            <ContentBody>
-                <DataInput>
-                    <LabelInput label="전시회 제목">
-                        <Input type="text" />
-                    </LabelInput>
-                    <LabelInput label="기간">
-                        <Input type="date" />
-                        <Label>부터</Label>
-                        <Input type="date" />
-                        <Label>까지</Label>
-                    </LabelInput>
-                    <LabelInput label="카테고리">
-                        {/* @TODO 카테고리 드랍다운 */}
-                        <Input type="type" />
-                    </LabelInput>
-                    <LabelInput label="협업">
-                        <Input type="type" />
-                    </LabelInput>
-                    <LabelInput label="설명">
-                        <TextArea />
-                    </LabelInput>
-                    <LabelInput label="전시회 썸네일">
-                        <ThumbnailBox
-                            onClick={() => thumbnailRef.current!.click()}
-                        />
-                        <Input ref={thumbnailRef} type="file" hidden />
-                    </LabelInput>
-                </DataInput>
-                <ArtworkSelectorWrapper>
-                    <ArtworkSelectorHeader>
-                        <Label>작품 선택하기</Label>
-                        <Link href="#">작품 추가하기</Link>
-                    </ArtworkSelectorHeader>
-                    <ArtworkSelector>
-                        {/* @TODO 내 아트워크들 넣기 */}
-                    </ArtworkSelector>
-                </ArtworkSelectorWrapper>
-                <NextButton>Next</NextButton>
-            </ContentBody>
-        </Layout>
+        <div>
+            <Head>
+                <title>벽전 - 전시회 등록</title>
+            </Head>
+            <Layout>
+                <Title>
+                    <h1>Hold Exhibition</h1>
+                    <Description>나만의 전시회를 만들어 보세요!</Description>
+                </Title>
+                <Container>
+                    <Form />
+                    <ArtworkSelector />
+                    <NextButton>Next</NextButton>
+                </Container>
+            </Layout>
+        </div>
     );
 };
+
+const Container = styled.div`
+    display: flex;
+    position: relative;
+
+    width: 920px;
+    margin: 50px auto;
+`;
+
+const Title = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    width: 920px;
+    margin: 0 auto;
+    margin-top: 40px;
+
+    & > h1 {
+        font: ${(props) => props.theme.font.textEnLg};
+        color: ${(props) => props.theme.color.placeholder};
+        margin-bottom: 8px;
+        margin: 0;
+    }
+`;
 
 export default ExhibitionPostPage;
