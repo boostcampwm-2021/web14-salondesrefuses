2d6052078a712b2ca7df0727ff2b2cee5a2fef2c
illlama <beatlemaniakih@gmail.com>
2021-11-11 21:06:25 +0900

    feat: Resize 기본적인거 완료 - #134
    - Resize는 동작합니다.
    - 점 이동해줘야함
    - 텍스트, 이미지도 적용해줘야함

---
+++ client/components/Exhibition/Editor/resizeFunctions.ts
@@ -0,0 +1,194 @@
+import { directionNames } from './utils';
+
+export const dirctionToResize = (
+    direction: directionNames,
+    element: HTMLElement | null,
+    ev: MouseEvent,
+    currentHeight: number,
+    currentWidth: number,
+    top: number,
+    left: number,
+    originPoint: number[],
+) => {
+    resizeFuncObj[direction](
+        element,
+        ev,
+        currentHeight,
+        currentWidth,
+        top,
+        left,
+        originPoint,
+    );
+};
+
+const NWResize = (
+    element: HTMLElement | null,
+    ev: MouseEvent,
+    currentHeight: number,
+    currentWidth: number,
+    top: number,
+    left: number,
+    originPoint: number[],
+) => {
+    if (!element) return;
+    element.style.setProperty(
+        'top',
+        `${top + (ev.clientY - originPoint[1])}px`,
+    );
+    element.style.setProperty(
+        'left',
+        `${left + (ev.clientX - originPoint[0])}px`,
+    );
+    element.style.setProperty(
+        'height',
+        `${currentHeight - (ev.clientY - originPoint[1])}px`,
+    );
+    element.style.setProperty(
+        'width',
+        `${currentWidth - (ev.clientX - originPoint[0])}px`,
+    );
+};
+const NResize = (
+    element: HTMLElement | null,
+    ev: MouseEvent,
+    currentHeight: number,
+    currentWidth: number,
+    top: number,
+    left: number,
+    originPoint: number[],
+) => {
+    if (!element) return;
+    element.style.setProperty(
+        'top',
+        `${top + (ev.clientY - originPoint[1])}px`,
+    );
+    element.style.setProperty(
+        'height',
+        `${currentHeight - (ev.clientY - originPoint[1])}px`,
+    );
+};
+const NEResize = (
+    element: HTMLElement | null,
+    ev: MouseEvent,
+    currentHeight: number,
+    currentWidth: number,
+    top: number,
+    left: number,
+    originPoint: number[],
+) => {
+    if (!element) return;
+    element.style.setProperty(
+        'top',
+        `${top + (ev.clientY - originPoint[1])}px`,
+    );
+    element.style.setProperty(
+        'height',
+        `${currentHeight - (ev.clientY - originPoint[1])}px`,
+    );
+    element.style.setProperty(
+        'width',
+        `${currentWidth + (ev.clientX - originPoint[0])}px`,
+    );
+};
+const EResize = (
+    element: HTMLElement | null,
+    ev: MouseEvent,
+    currentHeight: number,
+    currentWidth: number,
+    top: number,
+    left: number,
+    originPoint: number[],
+) => {
+    if (!element) return;
+
+    element.style.setProperty(
+        'width',
+        `${currentWidth + (ev.clientX - originPoint[0])}px`,
+    );
+};
+const SEResize = (
+    element: HTMLElement | null,
+    ev: MouseEvent,
+    currentHeight: number,
+    currentWidth: number,
+    top: number,
+    left: number,
+    originPoint: number[],
+) => {
+    if (!element) return;
+    element.style.setProperty(
+        'height',
+        `${currentHeight + (ev.clientY - originPoint[1])}px`,
+    );
+    element.style.setProperty(
+        'width',
+        `${currentWidth + (ev.clientX - originPoint[0])}px`,
+    );
+};
+const SResize = (
+    element: HTMLElement | null,
+    ev: MouseEvent,
+    currentHeight: number,
+    currentWidth: number,
+    top: number,
+    left: number,
+    originPoint: number[],
+) => {
+    if (!element) return;
+    element.style.setProperty(
+        'height',
+        `${currentHeight + (ev.clientY - originPoint[1])}px`,
+    );
+};
+const SWResize = (
+    element: HTMLElement | null,
+    ev: MouseEvent,
+    currentHeight: number,
+    currentWidth: number,
+    top: number,
+    left: number,
+    originPoint: number[],
+) => {
+    if (!element) return;
+    element.style.setProperty(
+        'left',
+        `${left + (ev.clientX - originPoint[0])}px`,
+    );
+    element.style.setProperty(
+        'height',
+        `${currentHeight + (ev.clientY - originPoint[1])}px`,
+    );
+    element.style.setProperty(
+        'width',
+        `${currentWidth - (ev.clientX - originPoint[0])}px`,
+    );
+};
+const WResize = (
+    element: HTMLElement | null,
+    ev: MouseEvent,
+    currentHeight: number,
+    currentWidth: number,
+    top: number,
+    left: number,
+    originPoint: number[],
+) => {
+    if (!element) return;
+    element.style.setProperty(
+        'left',
+        `${left + (ev.clientX - originPoint[0])}px`,
+    );
+    element.style.setProperty(
+        'width',
+        `${currentWidth - (ev.clientX - originPoint[0])}px`,
+    );
+};
+const resizeFuncObj = {
+    NW: NWResize,
+    N: NResize,
+    NE: NEResize,
+    E: EResize,
+    SE: SEResize,
+    S: SResize,
+    SW: SWResize,
+    W: WResize,
+};
