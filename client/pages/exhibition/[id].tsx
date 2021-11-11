35a1adf148d212baa42d17d7503ddf25c1b97dcd
imnotmoon <imnotmoon@gmail.com>
2021-11-11 19:47:36 +0900

    feat: exhibition 상세 페이지 레이아웃만 구성

--- client/pages/exhibition/[id].tsx
+++ client/pages/exhibition/[id].tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import Head from 'next/head';
+
+import Layout from '@components/common/Layout';
 
 const ExhibitionDetailPage = () => {
-    return <div></div>;
+    return (
+        <div>
+            <Head>
+                <title>벽전 - 전시회 탐색</title>
+            </Head>
+            <Layout>You're on 'Ehxibition Page'</Layout>
+        </div>
+    );
 };
 
 export default ExhibitionDetailPage;
