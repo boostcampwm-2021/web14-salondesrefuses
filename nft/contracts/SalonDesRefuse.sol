7e315171f487a5f3694e36821dd8812021a3faba
Alencion <rhqnrdl625@gmail.com>
2021-11-16 17:47:15 +0900

    feat: 경매 로직 추가

--- nft/contracts/SalonDesRefuse.sol
+++ nft/contracts/SalonDesRefuse.sol
@@ -10,7 +10,7 @@ contract SalonDesRefuse is ERC721, Ownable {
     Counters.Counter private _tokenIds;
 
     mapping(uint => string) tokenURIMap;
-    // mapping(tokenId => address) buyerAddress;
+    mapping(uint => uint) tokenPrice;
 
     constructor() ERC721("SalonDesRefuse", "SDR") public {
     }
@@ -34,7 +34,18 @@ contract SalonDesRefuse is ERC721, Ownable {
     }
     
     // 경매 로직 어떻게 할건지 생각해야겠다.
-//     function buyToken(uint _tokenId) external payable {
-//         require(buyerAddress[_tokenId])
-//   }
+    function buyToken(uint _tokenId, uint _price) external payable {
+        tokenPrice[_tokenId] = msg.value;
+    }
+
+    function getPrice(uint _tokenId) external view returns(uint){
+        require(ownerOf(_tokenId) == msg.sender);
+        return tokenPrice[_tokenId];
+    }
+
+    function transferToken(address payable from, address to, uint _tokenId) public {
+        require(ownerOf(_tokenId) == msg.sender);
+        from.transfer(tokenPrice[_tokenId]);
+        safeTransferFrom(msg.sender, to, _tokenId);
+    }
 }
--- nft/scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'test'
-
-npm install
-npm run deploy
-
-mv ./build/contracts/SalonDesRefuse.json ../client/public/etherium/abi.json
-mv ./build/address/SalonDesRefusesAddress.json ../client/public/etherium/address.json
