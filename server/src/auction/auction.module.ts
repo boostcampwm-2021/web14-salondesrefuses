7b768a99f94f6f75bb2504643b1a0578dc448a22
Alencion <rhqnrdl625@gmail.com>
2021-11-18 10:45:14 +0900

    fix: 안쓰는 코드 제거

--- server/src/auction/auction.module.ts
+++ server/src/auction/auction.module.ts
@@ -5,17 +5,9 @@ import { AuctionRepository } from './auction.repository';
 import { AuctionHistoryModule } from '../auctionHistory/auctionHistory.module';
 import AuctionController from './controller/auction.controller';
 import AuctionService from './service/auction.service';
-import { JwtModule } from '@nestjs/jwt';
-import { UserRepository } from 'src/user/user.repository';
 
 @Module({
-    imports: [
-        AuctionHistoryModule,
-        TypeOrmModule.forFeature([AuctionRepository, UserRepository]),
-        JwtModule.register({
-            secret: process.env.JWT_SECRET_KEY,
-        }),
-    ],
+    imports: [AuctionHistoryModule, TypeOrmModule.forFeature([AuctionRepository])],
     controllers: [AuctionController],
     providers: [AuctionService, AuctionGateway],
     exports: [AuctionService],
--- server/src/cronTask/crontask.service.ts
+++ server/src/cronTask/crontask.service.ts
@@ -1,13 +1,11 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import { ArtworkStatus } from 'src/artwork/artwork.status.enum';
 import { ArtworkService } from 'src/artwork/service/artwork.service';
 import AuctionService from 'src/auction/service/auction.service';
 
 @Injectable()
 export class CronTaskService {
     private readonly logger = new Logger(CronTaskService.name);
-    private readonly modular = 1000 * 60 * 60 * 24;
 
     constructor(private readonly auctionService: AuctionService, private readonly artworkService: ArtworkService) {}
 
