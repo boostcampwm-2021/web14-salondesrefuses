5189cf06d81d1bd37a362dcc1e67b49c7ccaab60
imnotmoon <imnotmoon@gmail.com>
2021-11-18 17:46:33 +0900

    feat: mypage에서 사용할 filter 컴포넌트 생성

--- client/components/MyPage/AuctionPage/index.tsx
+++ client/components/MyPage/AuctionPage/index.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+import Filter from '../Filter';
+
+const filtering = ['거래', '입찰'];
 
 const AuctionPage = () => {
-    return <div></div>;
+    const [filter, setFilter] = useState<string>('거래');
+    return (
+        <Container>
+            <Filter
+                filtering={filtering}
+                current={filter}
+                filterHandler={(s: string) => {
+                    setFilter(s);
+                }}
+            />
+        </Container>
+    );
 };
+
+const Container = styled.div``;
 
 export default AuctionPage;
--- client/components/MyPage/Filter/index.tsx
+++ client/components/MyPage/Filter/index.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
-const Filter = () => {
-    return <div></div>;
+interface IFilter {
+    filtering: string[];
+    current: string;
+    filterHandler: (s: string) => void;
+}
+
+const Filter = ({ filtering, current, filterHandler }: IFilter) => {
+    const onClickItem = (item: string) => {
+        return (e: React.MouseEvent) => {
+            filterHandler(item);
+        };
+    };
+
+    return (
+        <Container>
+            {filtering.map((item) => {
+                return (
+                    <Item onClick={onClickItem(item)} active={current === item}>
+                        {item}
+                    </Item>
+                );
+            })}
+        </Container>
+    );
 };
+
+const Container = styled.div`
+    height: 30px;
+    display: flex;
+
+    & > div:nth-of-type(2) {
+        border: none;
+    }
+`;
+
+const Item = styled.div<{ active: boolean }>`
+    font: ${(props) => props.theme.font.textMd};
+    color: ${(props) => (props.active ? props.theme.color.primary : props.theme.color.placeholder)};
+    padding: 0 20px;
+    border-right: 1px solid #a6a6a6;
+    cursor: pointer;
+`;
 
 export default Filter;
