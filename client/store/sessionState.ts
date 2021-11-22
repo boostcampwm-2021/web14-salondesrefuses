14cec2dea3f05b2e3cb73aea9e88d973d3da5043
imnotmoon <imnotmoon@gmail.com>
2021-11-22 17:07:18 +0900

    fix: mint 여러번 불가능하도록 수정

--- client/store/sessionState.ts
+++ client/store/sessionState.ts
@@ -5,7 +5,6 @@ import { getUser } from '@utils/networking';
 const sessionSelector = selector({
     key: '@session/get',
     get: async ({ get }) => {
-        console.log(parseCookie()('refreshToken'));
         if (!parseCookie()('refreshToken')) return undefined;
         const result = await getUser();
         return result;
