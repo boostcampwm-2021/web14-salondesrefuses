82771b910c1b1241f784ae78ba6883c6b6bed821
Alencion <rhqnrdl625@gmail.com>
2021-11-24 22:59:24 +0900

    chore: nft ropsten 으로 배포 도전

--- nft/truffle-config.js
+++ nft/truffle-config.js
@@ -1,4 +1,8 @@
 require("dotenv").config();
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const infuraKey = "c9d0de7fc3a24095a4e00ecbfdc2fd25";
+const mnemonic = process.env.MNEMONIC.toString().trim();
+
 module.exports = {
   networks: {
     development: {
@@ -17,14 +21,18 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://ropsten.infura.io/v3/${infuraKey}`
+        ),
+      network_id: 3, // Ropsten's id
+      gas: 5500000, // Ropsten has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
