412bc393942ee1a24ce6fcd947926ce56f200df3
imnotmoon <imnotmoon@gmail.com>
2021-11-25 15:28:41 +0900

    feat: curation 정적 데이터 추가

--- client/components/Auction/CarouselItem.tsx/index.tsx
+++ client/components/Auction/CarouselItem.tsx/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import createResource from '@utils/createResource';
+import { getSingleArtwork } from 'service/networking';
 
 interface Curation {
     id: number;
     title: string;
     content: string;
+    artworks: number[];
+    url: string;
     thumbnailImage: string;
 }
 
@@ -12,6 +16,9 @@ const CarouselItem = ({ curation }: { curation: Curation }) => {
     return <Container></Container>;
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+    width: 100%;
+    height: 100%;
+`;
 
 export default CarouselItem;
---
+++ client/constants/curations.ts
@@ -0,0 +1,40 @@
+const curations = [
+    {
+        id: 1,
+        title: 'New Photograph of George The JOKDURI',
+        content: '족두리봉의 죨-지가 새로운 작품을 소개합니다.',
+        artworks: [1],
+        url: '/auction/curation/george',
+        thumbnailImage:
+            'https://lh3.googleusercontent.com/YMHy5itMRP82Ocy5E-FLT8Ns3brcwzNdSMdBE9CQNKDog1mwo1XRWGGs7mEVMZB_1LgNLJpZNwTHAIDJE-IwEGGt0b7y-4wC2hog=w600',
+    },
+    {
+        id: 2,
+        title: 'imnotmoon 작가의 짱구 콜렉션',
+        content: '앗! 짱구가 타이어보다 싸다!',
+        artworks: [2, 3, 4],
+        url: '/auction/curation/imnotmoon',
+        thumbnailImage:
+            'https://lh3.googleusercontent.com/BjkYjPnOeUYYDNs2B3Qp7p4r4YxofZM7c5_iamfe6Tefr7JKsQqUZYTSfXzhOzAyE5TS3C4wWbuSUFFAL-vU2knFJ-h-OdeKC92gqA=w600',
+    },
+    {
+        id: 3,
+        title: '오늘이 가장 싼 작품들!',
+        content: '회원님에게만 특별한 가격에 드려요!',
+        artworks: [1, 2, 4, 5],
+        url: '/auction/curation/reasonable',
+        thumbnailImage:
+            'https://lh3.googleusercontent.com/YMHy5itMRP82Ocy5E-FLT8Ns3brcwzNdSMdBE9CQNKDog1mwo1XRWGGs7mEVMZB_1LgNLJpZNwTHAIDJE-IwEGGt0b7y-4wC2hog=w600',
+    },
+    {
+        id: 4,
+        title: '1ETH 이하의 저렴한 작품들',
+        content: '1ETH는 낼만 하잖아요 그쵸?',
+        artworks: [3, 5, 6, 7, 8],
+        url: '/auction/curation/series',
+        thumbnailImage:
+            'https://lh3.googleusercontent.com/dPiqY8uf3EKAz-W3VPX_RxkARRyu1Rv7ZArd8HhyGfaaR1T87JJjnxRwey1RzCJ2PpJxCSl_2VpxloHIQOJi6ivQ2ghdzOD6bqEk-g=w600',
+    },
+];
+
+export default curations;
