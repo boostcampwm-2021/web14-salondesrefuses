a21b1d72690b4e5afc7ef5089ef23de5c19c0429
Alencion <rhqnrdl625@gmail.com>
2021-11-29 16:12:34 +0900

    chore: nft migration 다시 살림.
    불필요한 친구줄 알았던 Migration.sol이 truffle deploy 시 기존의 compile
    내용 가지고 배포해주는 친구였습니다. 무지에서 나온 버그 였습니다.

--- nft/deploy_dev.sh
+++ nft/deploy_dev.sh
@@ -3,9 +3,9 @@
 CURRENT_PATH=`pwd -P`
 NFT_PATH=${CURRENT_PATH%/*}
 
-echo '> nft 배포'
+echo '> nft 빌드'
 
-truffle deploy
+truffle migrate --network development
 
 if [ ! -d $NFT_PATH/server/src/cronTask/ethereum ]; then
     mkdir $NFT_PATH/server/src/cronTask/ethereum
@@ -15,10 +15,10 @@ if [ ! -d $NFT_PATH/client/public/ethereum ]; then
     mkdir $NFT_PATH/client/public/ethereum
 fi
 
-cp $NFT_PATH/nft/build/address/SalonDesRefusesAddress.json $NFT_PATH/server/src/cronTask/ethereum/address.json
-cp $NFT_PATH/nft/build/address/SalonDesRefusesAddress.json $NFT_PATH/client/public/ethereum/address.json
+cp $NFT_PATH/nft/build/address/devAddress.json $NFT_PATH/server/src/cronTask/ethereum/address.json
+cp $NFT_PATH/nft/build/address/devAddress.json $NFT_PATH/client/public/ethereum/address.json
 
 cp $NFT_PATH/nft/build/contracts/SalonDesRefuse.json $NFT_PATH/server/src/cronTask/ethereum/abi.json
 cp $NFT_PATH/nft/build/contracts/SalonDesRefuse.json $NFT_PATH/client/public/ethereum/abi.json
 
-echo '> nft 배포 끝'
+echo '> nft 빌드끝'
