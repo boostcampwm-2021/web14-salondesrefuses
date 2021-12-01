b70a5bab7d5ea9c629548399dbd45e2e46405c3f
kykapple <kykapple@naver.com>
2021-12-01 15:57:32 +0900

    refactor: category swagger 데코레이터 커스텀

--- server/src/category/category.controller.ts
+++ server/src/category/category.controller.ts
@@ -1,27 +1,22 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
-import { ApiBody, ApiOperation, ApiProperty, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { CustomAuthGuard } from 'src/auth/guard/CustomAuthGuard';
+import { Get, Post, Body, UseGuards } from '@nestjs/common';
+import { _CategoryController, GetCategoriesApi, PostCategoriesApi } from './decorator';
 import { Category } from './category.entity';
 import { CategoryService } from './category.service';
-import { postCategoriesApiBody } from './swagger';
+import { CustomAuthGuard } from 'src/auth/guard/CustomAuthGuard';
 
-@Controller('categories')
-@ApiTags('카테고리 컨트롤러')
-export class CategoryContoller {
+@_CategoryController()
+export class CategoryController {
     constructor(private readonly categoryService: CategoryService) {}
 
     @Get()
-    @ApiOperation({ summary: '카테고리 조회 API' })
-    @ApiResponse({ type: [Category] })
+    @GetCategoriesApi()
     getCategories(): Promise<Category[]> {
         return this.categoryService.getCategories();
     }
 
     @Post()
     @UseGuards(CustomAuthGuard)
-    @ApiOperation({ summary: '카테고리 생성 API' })
-    @ApiProperty({ type: Category })
-    @ApiBody(postCategoriesApiBody)
+    @PostCategoriesApi()
     postCategories(@Body('name') name: string): Promise<Category> {
         return this.categoryService.createCategory(name);
     }
